b2dc9c7a87bbc323019639cf89d1e1580607ddd4
Muhammad Luthfi Pratama Putra <86417070+Piluthfi@users.noreply.github.com>
2021-10-21 09:56:25 +0700

    Update soal3.java

--- soal3.java
+++ soal3.java
@@ -1,5 +1,5 @@
 class soal3{
-    public static void main(String[] argumen){
+    public static void main(String[] args){
         for(int i=1; i <= 10; i++){
             System.out.print( i + " " );
         }
